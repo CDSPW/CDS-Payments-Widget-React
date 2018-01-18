0e6ed83210939024f2f33d324eb0317f55a472e5
JoshMcCall <joshmccall221@gmail.com>
2018-01-17 21:12:52 -0600

    remove links

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, NavLink, Route } from 'react-router-dom';
-import HomePage from './HomePage';
+import { Switch,  Route } from 'react-router-dom';
 import FuelSavingsPage from './containers/FuelSavingsPage';
-import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 
 // This is a class-based component because the current
@@ -13,24 +11,10 @@ import NotFoundPage from './NotFoundPage';
 
 class App extends React.Component {
   render() {
-    const activeStyle = { color: 'blue' };
     return (
       <div>
-
-        {false &&
-        <div>
-          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
-          {' | '}
-          <NavLink to="/fuel-savings" activeStyle={activeStyle}>Demo App</NavLink>
-          {' | '}
-          <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </div>
-      }
         <Switch>
           <Route exact path="/" component={FuelSavingsPage} />
-          <Route exact path="/home" component={HomePage} />
-          <Route path="/fuel-savings" component={FuelSavingsPage} />
-          <Route path="/about" component={AboutPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </div>

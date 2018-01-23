de75017e726cc8f392376a3c96651b82c871f272
JoshMcCall <joshmccall221@gmail.com>
2018-01-22 23:24:03 -0600

    updates

--- tools/go.sh
+++ tools/go.sh
@@ -1,6 +1,7 @@
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 unset npm_config_prefix
  . ~/.nvm/nvm.sh
+ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
  nvm i
  nvm use
  npm i

89c2891770b069eace965fd20e6ee6916f8287a5
JoshMcCall <joshmccall221@gmail.com>
2018-01-25 11:58:17 -0600

    updates

--- src/components/FuelSavingsForm.js
+++ src/components/FuelSavingsForm.js
@@ -31,6 +31,195 @@ const FuelSavingsForm = ({ fuelSavings, onChange }) => {
   return (
     <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
       <div>
+        <br /> <br /> <br />
+        <br /> <br /> <br />
+        <Card
+          style={{ width: 650 }}
+        >
+          <CardTitle title="Choose your preferred payment option:" />
+          <List>
+            {
+              [
+                fuelSavings.yourWallet &&
+                <div>
+                  <CardTitle
+                    subtitle="Your wallet:"
+                    style={{
+                      display: 'inline-block',
+                      verticalAlign: 'top'
+                    }}
+                  />
+                  <DropDownMenu
+                    value={1}
+                    onChange={() => undefined}
+                  >
+                    <MenuItem value={1} primaryText="MasterCard" />
+                    <MenuItem value={2} primaryText="Visa" />
+                    <MenuItem value={3} primaryText="Discover" />
+                    <MenuItem value={4} primaryText="AmericanExpress" />
+                  </DropDownMenu>
+                  <Divider />
+                </div>,
+                fuelSavings.payWith && <div>
+                  <RaisedButton
+                    label="Apple Pay"
+                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
+                    style={{ margin: 5 }}
+                  />
+                  <RaisedButton
+                    label="PayPal"
+                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
+                    style={{ margin: 5 }}
+                  />
+                  <RaisedButton
+                    label="Amazon Pay"
+                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
+                    style={{ margin: 5 }}
+                  />
+                  <CardTitle
+                    subtitle="(You will be prompted to sign in to complete the payment process)"
+                    style={{
+                      display: 'inline-block',
+                      verticalAlign: 'top'
+                    }}
+                  />
+                  <Divider />
+                </div>,
+                fuelSavings.credit && <div>
+                  <RaisedButton
+                    label="Visa"
+                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
+                    style={{ margin: 5 }}
+                    disabled={niceType !== 'Visa'}
+                  />
+                  <RaisedButton
+                    label="MasterCard"
+                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
+                    style={{ margin: 5 }}
+                    disabled={niceType !== 'MasterCard'}
+                  />
+                  <RaisedButton
+                    label="Discover"
+                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
+                    style={{ margin: 5 }}
+                    disabled={niceType !== 'Discover'}
+                  />
+                  <RaisedButton
+                    label="American Express"
+                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
+                    style={{ margin: 5 }}
+                    disabled={niceType !== 'AmericanExpress'}
+                  />
+                  <div
+                    style={{
+                      //height: 100,
+                      //position: 'relative',
+                      //float: 'left'
+                    }}
+                  >
+                    <TextField
+                      style={{
+                        //margin: 5,
+                        width: '75%',
+                      }}
+                      inputStyle={{
+                        // height: 80,
+                      }}
+                      floatingLabelText="Card Number"
+                      onChange={(e, value) => onChange({ target: { name: 'ccNumber', value } })}
+                      value={ccNumber}
+                      name="ccNumber"
+                      id="cc-number"
+                      className="cc-number"
+                      data-cds="ccNumber"
+                      errorText={
+                        (() => {
+                          if (ccNumber !== "" && !isValid)
+                            return "This field is required. " + niceType + " cards must be " + cardLength + " long.";
+                        })()
+                      }
+                    />
+                  </div>
+                  <div
+                    style={{
+                      //height: 100,
+                      //position: 'relative',
+                      //float: 'left'
+                    }}
+                  >
+                    <TextField
+                      onChange={(e, expDateValue) => onChange({ target: { name: 'expDate', value: expDateValue } })}
+                      name="expDate"
+                      value={expDate}
+                      style={{
+                        //margin: 5,
+                        // display: 'inline-block'
+                      }}
+                      floatingLabelText="Expiration Date"
+                      hintText="MM/YY"
+                      errorText={
+                        (() => {
+                          if (ccNumber !== "" && !validExpDate)
+                            return "This field is required. ";
+                        })()
+                      }
+                    />
+                  </div>
+                  <div
+                  //style={{
+                  //  height: 100,
+                  //  position: 'relative',
+                  //  float: 'left'
+                  //}}
+                  >
+                    <TextField
+                      style={{
+                        //margin: 5,
+                        //width: 25,
+                        //display: 'inline-block'
+                      }}
+                      floatingLabelText="CVV"
+                      name="cvv"
+                      onChange={onChange}
+                      value={cvv}
+                      errorText={
+                        (() => {
+                          if (ccNumber !== "" && !validCvv) return niceType ? "not valid..." + niceType + " requires " + cvvLength + " digits." : "not valid...";
+                        })()
+                      }
+                    />
+                    <br /><br />
+                  </div>
+                  {fuelSavings.yourWallet && <Checkbox
+                    label="Save this card to My Wallet"
+                  />}
+                </div>,
+                fuelSavings.billMe && <div>
+                  <Divider />
+                  <br /> <br />
+                  <Checkbox
+                    label="Bill me later"
+                  />
+                </div>
+              ]
+                .filter(f => f)
+                .map((e, index, options) =>
+                  (<ListItem
+                    leftAvatar={(options.length > 1) && <Avatar style={{ marginTop: '10px' }} size={35}>
+                      {options.length > 1 ? index + 1 : ''}
+                      {console.log("\n\n\n", { options }, "\n\n", options.length)}
+                    </Avatar>}
+                    disabled
+                    key={index}
+                  >
+                    {e}
+                  </ListItem>)
+                )
+            }
+          </List>
+        </Card>
+
+        <br /> <br /> <br />
         <Drawer
           open
         >
@@ -267,195 +456,6 @@ const FuelSavingsForm = ({ fuelSavings, onChange }) => {
 
           </Card>
         </Drawer>
-        <br /> <br /> <br />
-        <br /> <br /> <br />
-        <Card
-          style={{ width: 650 }}
-        >
-          <CardTitle title="Choose your preferred payment option:" />
-          <List>
-            {
-              [
-                fuelSavings.yourWallet &&
-                <div>
-                  <CardTitle
-                    subtitle="Your wallet:"
-                    style={{
-                      display: 'inline-block',
-                      verticalAlign: 'top'
-                    }}
-                  />
-                  <DropDownMenu
-                    value={1}
-                    onChange={() => undefined}
-                  >
-                    <MenuItem value={1} primaryText="MasterCard" />
-                    <MenuItem value={2} primaryText="Visa" />
-                    <MenuItem value={3} primaryText="Discover" />
-                    <MenuItem value={4} primaryText="AmericanExpress" />
-                  </DropDownMenu>
-                  <Divider />
-                </div>,
-                fuelSavings.payWith && <div>
-                  <RaisedButton
-                    label="Apple Pay"
-                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
-                    style={{ margin: 5 }}
-                  />
-                  <RaisedButton
-                    label="PayPal"
-                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
-                    style={{ margin: 5 }}
-                  />
-                  <RaisedButton
-                    label="Amazon Pay"
-                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
-                    style={{ margin: 5 }}
-                  />
-                  <CardTitle
-                    subtitle="(You will be prompted to sign in to complete the payment process)"
-                    style={{
-                      display: 'inline-block',
-                      verticalAlign: 'top'
-                    }}
-                  />
-                  <Divider />
-                </div>,
-                fuelSavings.credit && <div>
-                  <RaisedButton
-                    label="Visa"
-                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
-                    style={{ margin: 5 }}
-                    disabled={niceType !== 'Visa'}
-                  />
-                  <RaisedButton
-                    label="MasterCard"
-                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
-                    style={{ margin: 5 }}
-                    disabled={niceType !== 'MasterCard'}
-                  />
-                  <RaisedButton
-                    label="Discover"
-                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
-                    style={{ margin: 5 }}
-                    disabled={niceType !== 'Discover'}
-                  />
-                  <RaisedButton
-                    label="American Express"
-                    icon={<FontIcon className="muidocs-icon-custom-payment" />}
-                    style={{ margin: 5 }}
-                    disabled={niceType !== 'AmericanExpress'}
-                  />
-                  <div
-                    style={{
-                      //height: 100,
-                      //position: 'relative',
-                      //float: 'left'
-                    }}
-                  >
-                    <TextField
-                      style={{
-                        //margin: 5,
-                        width: '75%',
-                      }}
-                      inputStyle={{
-                        // height: 80,
-                      }}
-                      floatingLabelText="Card Number"
-                      onChange={(e, value) => onChange({ target: { name: 'ccNumber', value } })}
-                      value={ccNumber}
-                      name="ccNumber"
-                      id="cc-number"
-                      className="cc-number"
-                      data-cds="ccNumber"
-                      errorText={
-                        (() => {
-                          if (ccNumber !== "" && !isValid)
-                            return "This field is required. " + niceType + " cards must be " + cardLength + " long.";
-                        })()
-                      }
-                    />
-                  </div>
-                  <div
-                    style={{
-                      //height: 100,
-                      //position: 'relative',
-                      //float: 'left'
-                    }}
-                  >
-                    <TextField
-                      onChange={(e, expDateValue) => onChange({ target: { name: 'expDate', value: expDateValue } })}
-                      name="expDate"
-                      value={expDate}
-                      style={{
-                        //margin: 5,
-                        // display: 'inline-block'
-                      }}
-                      floatingLabelText="Expiration Date"
-                      hintText="MM/YY"
-                      errorText={
-                        (() => {
-                          if (ccNumber !== "" && !validExpDate)
-                            return "This field is required. ";
-                        })()
-                      }
-                    />
-                  </div>
-                  <div
-                  //style={{
-                  //  height: 100,
-                  //  position: 'relative',
-                  //  float: 'left'
-                  //}}
-                  >
-                    <TextField
-                      style={{
-                        //margin: 5,
-                        //width: 25,
-                        //display: 'inline-block'
-                      }}
-                      floatingLabelText="CVV"
-                      name="cvv"
-                      onChange={onChange}
-                      value={cvv}
-                      errorText={
-                        (() => {
-                          if (ccNumber !== "" && !validCvv) return niceType ? "not valid..." + niceType + " requires " + cvvLength + " digits." : "not valid...";
-                        })()
-                      }
-                    />
-                    <br /><br />
-                  </div>
-                  {fuelSavings.yourWallet && <Checkbox
-                    label="Save this card to My Wallet"
-                  />}
-                </div>,
-                fuelSavings.billMe && <div>
-                  <Divider />
-                  <br /> <br />
-                  <Checkbox
-                    label="Bill me later"
-                  />
-                </div>
-              ]
-                .filter(f => f)
-                .map((e, index, options) =>
-                  (<ListItem
-                    leftAvatar={(options.length > 1) && <Avatar style={{ marginTop: '10px' }} size={35}>
-                      {options.length > 1 ? index + 1 : ''}
-                      {console.log("\n\n\n", { options }, "\n\n", options.length)}
-                    </Avatar>}
-                    disabled
-                    key={index}
-                  >
-                    {e}
-                  </ListItem>)
-                )
-            }
-          </List>
-        </Card>
-
-        <br /> <br /> <br />
       </div>
     </MuiThemeProvider>
   );
